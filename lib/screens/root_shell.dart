9617ba342fc65d81c89503715f8d9074095d6296
Abdairsyaputramirudin <abdairsya03122004@gmail.com>
2025-12-19 19:32:30 +0700

    style bar bawah

--- lib/screens/root_shell.dart
+++ lib/screens/root_shell.dart
@@ -21,14 +21,42 @@ class _RootShellState extends State<RootShell> {
 
     return Scaffold(
       body: pages[index],
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: index,
-        onDestinationSelected: (v) => setState(() => index = v),
-        destinations: const [
-          NavigationDestination(icon: Icon(Icons.home_outlined), label: "Home"),
-          NavigationDestination(
-              icon: Icon(Icons.person_outline), label: "Profil"),
-        ],
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          navigationBarTheme: NavigationBarThemeData(
+            backgroundColor: Colors.white,
+            surfaceTintColor: Colors.white,
+            indicatorColor: const Color(0xFFE9EEF9),
+            labelTextStyle: WidgetStateProperty.resolveWith((states) {
+              if (states.contains(WidgetState.selected)) {
+                return const TextStyle(
+                    fontWeight: FontWeight.w800,
+                    fontSize: 12,
+                    color: Colors.blue);
+              }
+              return const TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 12,
+                  color: Colors.black54);
+            }),
+            iconTheme: WidgetStateProperty.resolveWith((states) {
+              if (states.contains(WidgetState.selected)) {
+                return const IconThemeData(color: Colors.blue);
+              }
+              return const IconThemeData(color: Colors.black54);
+            }),
+          ),
+        ),
+        child: NavigationBar(
+          selectedIndex: index,
+          onDestinationSelected: (v) => setState(() => index = v),
+          destinations: const [
+            NavigationDestination(
+                icon: Icon(Icons.home_outlined), label: "Home"),
+            NavigationDestination(
+                icon: Icon(Icons.person_outline), label: "Profil"),
+          ],
+        ),
       ),
     );
   }
